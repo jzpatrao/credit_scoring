407a41d13e4e1291e7f01c5baa8ed2923e8201c2
Qingqing <zheng.qingq@gmail.com>
2023-04-04 17:41:21 +0200

    create hello world http server

---
+++ hello.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+from http import HTTPStatus
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b'Hello world')
+
+
+httpd = socketserver.TCPServer(('', 8000), Handler)
+httpd.serve_forever()
+
+
+# python3 server.py
+# 127.0.0.1 - - [11/Apr/2017 11:36:49] "GET / HTTP/1.1" 200 -
+# http :8000
