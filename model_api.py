6e67c664df893e4bde556e591b5863b8d9361af4
Qingqing <zheng.qingq@gmail.com>
2023-04-04 15:35:49 +0200

    Update model_api.py
    change host from host="0.0.0.0", port=8080" to host="127.0. 0.1", port=8080"

--- model_api.py
+++ model_api.py
@@ -41,7 +41,7 @@ async def give_score(data:ClientFeatures):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0. 0.1", port=8080)
 
 # uvicorn app:app --reload
                       
