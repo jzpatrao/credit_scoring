f3983aad84d327ded38b4edf56329f809ce63941
Qingqing <zheng.qingq@gmail.com>
2023-04-05 21:44:16 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
+import plotly.graph_objs as go
 
-@app.get('/')
-def hello():
-    return {'message': 'Hello, World'}
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
+
+app.layout = dbc.Container([
+    html.H1("Credit Scoring",className='text-center mb-4'),
+    
+    dbc.Row([])
+    
+    dbc.Row([]),
+    ],
+    fluid=True,
+)
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
