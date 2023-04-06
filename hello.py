d6b954326c47f29fc4a8bd403c2c7c4ba9757d3f
Qingqing <zheng.qingq@gmail.com>
2023-04-06 10:00:00 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
+import pandas as pd
 
 app = Dash(external_stylesheets=[dbc.themes.JOURNAL])
 
 server = app.server
 
+df = pd.read_csv("application_test.csv")
+#------------------------------------------------------------
+select_client = dbc.Card(
+    [html.Div([dbc.Label("Client ID"),
+               dcc.Dropdown(id="Client ID",options=df['SK_ID_CURR'].values,multi=False,value=100001)]),
+    ],body=True)
+
 app.layout = dbc.Container([
-    html.H1("Credit Scoring",className='text-center mb-4')
+    html.H1("Credit Scoring",className='text-center mb-4'),
+    dbc.Row([dbc.Col(select_client,width=12),])
+    
                             ])
 
 # @app.callback(Output('display-value', 'children'),
