c01f3e99295175b1fd7066c89c4c3db0b35c8757
Qingqing <zheng.qingq@gmail.com>
2023-04-06 10:42:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -12,12 +12,24 @@ select_client = dbc.Card(
     [html.Div([dbc.Label("Client ID"),
                dcc.Dropdown(id="Client ID",options=df['SK_ID_CURR'].values,multi=False,value=100001)]),
     ],body=True)
+client_profile=[dbc.Row(html.Div(title='Income type',id='Income type', children=[])),
+                dbc.Row(html.Div(title='Education type',id='Education type', children=[])),
+                dbc.Row(html.Div(title='Region rating client',id='Region rating client', children=[])),
+                dbc.Row(html.Div(title='Days birth',id='Days birth', children=[])),
+                dbc.Row(html.Div(title='Days employed percent',id='Days employed percent', children=[])),
+                dbc.Row(html.Div(title='Ext source 1',id='Ext source 1', children=[])),
+                dbc.Row(html.Div(title='Ext source 2',id='Ext source 2', children=[])),
+                dbc.Row(html.Div(title='Ext source 3',id='Ext source 3', children=[]))]
 
 app.layout = dbc.Container([
     html.H1("Credit Scoring",className='text-center mb-4'),
-    dbc.Row([dbc.Col(select_client,width=12),])
-    
-                            ])
+    dbc.Row([
+    dbc.Col(select_client,width=12),
+    dbc.Col(client_profile,width=12)],
+    className='text-center mb-4'),
+    ],
+    fluid=True,
+)
 
 
 if __name__ == '__main__':
