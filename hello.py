c4d237695683dc0655e60fb9ccc620aff53101d0
Qingqing <zheng.qingq@gmail.com>
2023-04-06 09:53:29 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,18 @@
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
-app = Dash(external_stylesheets=[dbc.themes.FLATLY])
+app = Dash(external_stylesheets=[dbc.themes.JOURNAL])
 
 server = app.server
 
-app.layout = html.Div([
-    html.H1('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
-    html.Div(id='display-value')
-])
+app.layout = dbc.Container([
+    html.H1("Credit Scoring",className='text-center mb-4')
+                            ])
 
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+# @app.callback(Output('display-value', 'children'),
+#                 [Input('dropdown', 'value')])
+# def display_value(value):
+#     return f'You have selected {value}'
 
 
 if __name__ == '__main__':
