51d19beffb471b7fb930c57237a1244ca7b6f2db
Qingqing <zheng.qingq@gmail.com>
2023-04-06 14:04:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -86,5 +86,4 @@ def filter_df(client_id):
     return Output1, Output2, Output3, Output4, Output5, Output6, Output7, Output8,shap_html
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8050))
-    app.run_server(debug=True, port=port)
+    app.run_server(debug=True,host='0.0.0.0')
