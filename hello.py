e443bfc1e8ef46dd1efffed59028ae3a1aad2445
Qingqing <zheng.qingq@gmail.com>
2023-04-06 09:46:44 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,20 @@ app = Dash(external_stylesheets=[dbc.themes.FLATLY])
 
 server = app.server
 
+app.layout = html.Div([
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
