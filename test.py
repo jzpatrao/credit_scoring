e0592c01433074fb050e74e932fc18548169eeb4
Qingqing <zheng.qingq@gmail.com>
2023-04-06 16:13:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -25,27 +25,39 @@ def get_force_plot_html(*args):
 client_options = df['SK_ID_CURR'].apply(lambda x: {'label': x, 'value': x}).to_list()
 
 select_client = html.Div([
-    dbc.Label("Client ID"),
-    dcc.Dropdown(id="client_id", options=client_options, multi=False, value=100001)
+dcc.Dropdown(id="client_id", options=client_options, multi=False, value=100001)
 ])
     
 
-client_profile = [
-	dbc.Row(html.Div(title='Income type', id='Income type', children=[])),
-	dbc.Row(html.Div(title='Education type',id='Education type', children=[])),
-    dbc.Row(html.Div(title='Region rating client',id='Region rating client', children=[])),
-    dbc.Row(html.Div(title='Days birth',id='Days birth', children=[])),
-    dbc.Row(html.Div(title='Days employed percent',id='Days employed percent', children=[])),
-    dbc.Row(html.Div(title='Ext source 1',id='Ext source 1', children=[])),
-    dbc.Row(html.Div(title='Ext source 2',id='Ext source 2', children=[])),
-    dbc.Row(html.Div(title='Ext source 3',id='Ext source 3', children=[]))
-]
+client_profile = html.Div([
+	dbc.Row([
+    html.Span("Income type: ", style={"font-weight": "bold"}),
+    html.Div(title='Income type', id='Income type', children=[])]),
+	dbc.Row([html.Span("Education type: ", style={"font-weight": "bold"}),
+          html.Div(title='Education type',id='Education type', children=[])]),
+    dbc.Row([html.Span("Region rating: ", style={"font-weight": "bold"}),
+             html.Div(title='Region rating client',id='Region rating client', children=[])]),
+    dbc.Row([html.Span("Days since birth: ", style={"font-weight": "bold"}),
+             html.Div(title='Days birth',id='Days birth', children=[])]),
+    dbc.Row([html.Span("Employment length: ", style={"font-weight": "bold"}),
+             html.Div(title='Days employed percent',id='Days employed percent', children=[])]),
+    dbc.Row([html.Span("Ext source 1: ", style={"font-weight": "bold"}),
+            html.Div(title='Ext source 1',id='Ext source 1', children=[])]),
+    dbc.Row([html.Span("Ext source 2: ", style={"font-weight": "bold"}),
+            html.Div(title='Ext source 2',id='Ext source 2', children=[])]),
+    dbc.Row([html.Span("Ext source: ", style={"font-weight": "bold"}),
+            html.Div(title='Ext source 3',id='Ext source 3', children=[])])
+])
 
 app.layout = dbc.Container([
     html.H1("Credit Scoring", className='text-center mb-4'),
     dbc.Row([
-        dbc.Col(dbc.Card(select_client), width=6),
-        dbc.Col(dbc.Card(client_profile), width=6)
+        dbc.Col(dbc.Card([dbc.CardHeader("Select Client ID"),
+                          select_client]),  
+                style={"width": "45%","height": "100%", "display": "inline-block", "vertical-align": "top"},),
+        dbc.Col(dbc.Card([dbc.CardHeader("Client Profile"),
+                          client_profile]),  
+                style={"width": "45%","height": "100%", "display": "inline-block", "vertical-align": "top"},)
     ], className='text-center mb-4'),
     dbc.Row([
         dbc.Row(html.Div(id='shap'))
@@ -74,18 +86,19 @@ def filter_df(client_id):
     data = df[df['SK_ID_CURR'] == client_id]
     idx = data.index.values
 
-    Output1 = data['NAME_INCOME_TYPE']
-    Output2 = data['NAME_EDUCATION_TYPE']
-    Output3 = data['REGION_RATING_CLIENT']
-    Output4 = data['DAYS_BIRTH']
-    Output5 = data['DAYS_EMPLOYED_PERC']
-    Output6 = data['EXT_SOURCE_1']
-    Output7 = data['EXT_SOURCE_2']
-    Output8 = data['EXT_SOURCE_3']
+    Output1 = html.Div(data['NAME_INCOME_TYPE'])
+    Output2 = html.Div(data['NAME_EDUCATION_TYPE'])
+    Output3 = html.Div(data['REGION_RATING_CLIENT'])
+    Output4 = html.Div(data['DAYS_BIRTH'])
+    Output5 = html.Div(data['DAYS_EMPLOYED_PERC'])
+    Output6 = html.Div(data['EXT_SOURCE_1'])
+    Output7 = html.Div(data['EXT_SOURCE_2'])
+    Output8 = html.Div(data['EXT_SOURCE_3'])
 
     shap_html = get_force_plot_html(shap_values[idx])
 
     return Output1, Output2, Output3, Output4, Output5, Output6, Output7, Output8, shap_html
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
