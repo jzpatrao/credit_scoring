4bcff56f5cbefcb075dae821ef5617e26ab354a0
Qingqing <zheng.qingq@gmail.com>
2023-04-11 10:33:36 +0200

    Update model.py

--- model.py
+++ model.py
@@ -2,13 +2,16 @@ import numpy as np
 import pickle
 import pandas as pd
 
+# Loading the trained model from a saved file
 with open("trained_pipeline.pkl", "rb") as f:
     model = pickle.load(f)
 
+# Defining a function to predict the risk score 
+#and application status of new data
 def get_score(data):
     score = model.predict_proba(data)[0, 1]
     score = round(score,2)
-    if score < 0.5:
+    if score < 0.4966:
         status = 'Accepted'
     else:
         status = 'Rejected'
